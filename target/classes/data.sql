12fb1df0318450df8bb9a263c62291000447395d
Stef80 <mide646@gmail.com>
2021-02-09 21:41:23 +0100

    aggiornamento

---
+++ target/classes/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (email, name, surname, password, workplace, expertise_area ) values
+('stefano.longobucco@agmsolutions.net', 'Stefano', 'Longobucco', '12345', 'Milano', 'jave be');
---
+++ target/classes/schema.sql
@@ -0,0 +1,52 @@
+CREATE TABLE Users(
+id BIGSERIAL PRIMARY KEY,
+email    VARCHAR(255) NOT NULL,
+name      VARCHAR(255) NOT NULL,
+surname   VARCHAR(255) NOT NULL,
+password  VARCHAR(255) NOT NULL,
+workplace VARCHAR(255) NOT NULL,
+expertise_area VARCHAR(255) NOT NULL);
+
+CREATE TABLE Roles(
+  id BIGSERIAL PRIMARY KEY,
+ user_id BIGINT NOT NULL,
+role VARCHAR(18) NOT NULL,
+FOREIGN KEY(user_id) REFERENCES Users ON DELETE CASCADE
+);
+
+
+CREATE TABLE Projects(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_days INT NOT NULL
+);
+
+CREATE TABLE Tasks(
+    id BIGSERIAL PRIMARY KEY,
+    task_name VARCHAR NOT NULL,
+    project_id BIGINT NOT NULL,
+    date_of_registation DATE NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    total_worked DECIMAL(2,2),
+    FOREIGN KEY(project_id) REFERENCES Projects  ON DELETE CASCADE
+);
+
+CREATE TABLE Assigned(
+id BIGSERIAL PRIMARY KEY,
+user_id BIGINT NOT NULL,
+task_id BIGINT NOT NULL,
+FOREIGN KEY(user_id) REFERENCES Users ON DELETE CASCADE,
+FOREIGN KEY(task_id) REFERENCES Tasks  ON DELETE CASCADE
+);
+
+CREATE TABLE Updates(
+     id  BIGSERIAL PRIMARY KEY ,
+     assigned_id BIGINT NOT NULL,
+     date_of_publish TIMESTAMP NOT NULL,
+     hours_of_working DECIMAL(3,2) NOT NULL,
+     FOREIGN KEY(assigned_id)REFERENCES Assigned ON DELETE CASCADE
+);
+
