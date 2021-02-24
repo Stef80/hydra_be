bb984fec493ed2c3eaa257ea9c99eaab14b033ed
Stef80 <mide646@gmail.com>
2021-02-24 08:50:49 +0100

    da eliminare

--- src/main/java/net/agm/hydra/config/AuthServerConfiguration.java
+++ src/main/java/net/agm/hydra/config/AuthServerConfiguration.java
@@ -14,31 +14,35 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 
 @Configuration
 public class AuthServerConfiguration implements AuthorizationServerConfigurer {
-	
+
 	@Value("${spring.security.oauth2.resourceserver.opaquetoken.client-id}")
 	private String clientId;
-	
+
 	@Value("${spring.security.oauth2.resourceserver.opaquetoken.client-secret}")
 	private String clientSecret;
 	@Autowired
 	PasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	DataSource data;
-	
+
 	@Autowired 
 	AuthenticationManager authenticationManager;
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-	 security.checkTokenAccess("isAutenticated()").tokenKeyAccess("permitAll()");
+		security.checkTokenAccess("isAuthenticated()").tokenKeyAccess("permitAll()");
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-	    clients.inMemory().withClient(clientId).secret(passwordEncoder.encode(clientSecret)).scopes("read", "write")
-	            .authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(20000)
-	            .refreshTokenValiditySeconds(20000);
+		clients.inMemory()
+		.withClient(clientId)
+		.secret(passwordEncoder.encode(clientSecret))
+		.scopes("read", "write")
+		.authorizedGrantTypes("client_credentials")
+		.accessTokenValiditySeconds(20000)
+		.refreshTokenValiditySeconds(20000);
 		//clients.jdbc(data).passwordEncoder(passwordEncoder);
 	}
 
@@ -46,8 +50,10 @@ public class AuthServerConfiguration implements AuthorizationServerConfigurer {
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(authenticationManager);
-		
+
 	}
+	
+	
 
 
 }
--- src/main/java/net/agm/hydra/config/WebSecurityCofiguration.java
+++ src/main/java/net/agm/hydra/config/WebSecurityCofiguration.java
@@ -46,6 +46,17 @@ public class WebSecurityCofiguration extends WebSecurityConfigurerAdapter {
 		web.ignoring();
 	}
 
+	   @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http
+	                .cors()
+	                .and()
+	                .csrf().disable()
+	                .authorizeRequests()
+	                .antMatchers("/api/user")
+	                .permitAll();
+	                
+	   }
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
